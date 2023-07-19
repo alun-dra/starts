22a2a10c69e4784bc9cb12d523670007ab110048
alvaro villalobos <alvaro.villalobos@social-it.cl>
2023-07-18 21:33:38 -0400

    arreglando importacion CLI

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,7 +1,7 @@
 
 import { Command } from 'commander';
-import createCommand from './commands/create';
-import startCommand from './commands/start';
+import createCommand from '../cli/commands/create.js';
+import startCommand from '../cli/commands/start.js';
 import { Component, Router } from '../core';
 
 // Configurar los comandos del CLI
