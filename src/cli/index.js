5435403f241bb847d4f10074c50a41f9f65cdb3b
alvaro villalobos <alvaro.villalobos@social-it.cl>
2023-07-20 00:30:27 -0400

    test

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,8 +1,8 @@
-
 import { Command } from 'commander';
 import createCommand from '../cli/commands/create.js';
 import startCommand from '../cli/commands/start.js';
-import { Component, Router } from '../core';
+import { Component } from '../../src/core/component.js';
+import { Router } from '../../src/core/router.js';
 
 // Configurar los comandos del CLI
 const program = new Command();
